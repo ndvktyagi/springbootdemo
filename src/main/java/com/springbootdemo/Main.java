433893b6c1a93377ccc543dbc2cc8f5574e4cdce
amitjattan <amitjattan@gmail.com>
2018-05-02 08:08:58 +0530

    changes

--- src/main/java/com/springbootdemo/Main.java
+++ src/main/java/com/springbootdemo/Main.java
@@ -6,7 +6,6 @@ import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
 public class Main extends SpringBootServletInitializer {
-
     @Override
     protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
         return application.sources(Main.class);
