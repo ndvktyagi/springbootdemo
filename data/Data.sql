0d95e4c2f48bb85234eb04edf9cf4e0fc59bc56d
amitjattan <amitjattan@gmail.com>
2018-05-02 18:28:49 +0530

    Test CI

--- data/Data.sql
+++ data/Data.sql
@@ -1,5 +1,5 @@
 /*drop table Student;
-create table Student (id int, name VARCHAR(20), classname VARCHAR(20));
+create table Student (id int, name VARCHAR(20), classname VARCHAR(20));*/
 
 SELECT * From student;
-commit;*/
+commit;
